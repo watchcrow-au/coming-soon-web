da55ae28b4e89c9c904fae5e8def1b06db7230a1
nfiny-jae <jae@nfiny.com>
2022-09-16 14:17:30 +1000

    updates to root level

---
+++ js/interestForm.js
@@ -0,0 +1,32 @@
+jQuery(document).ready(function ($) {
+  "use strict";
+
+  // Contact
+  $("form.interestForm").submit(function (e) {
+    alert(1);
+    e.preventDefault();
+
+    var action = $(this).attr("action");
+    var str = $(this).serialize();
+
+    $(".submit-loading").removeAttr("hidden");
+    $("#interestInput").hide();
+
+    $.ajax({
+      type: "POST",
+      url: action,
+      data: str,
+      success: function (msg) {
+        $(".submit-success").removeAttr("hidden");
+        $(".submit-loading").hide();
+        $("#interestInput").hide();
+      },
+      error: function (msg) {
+        $(".submit-error").removeAttr("hidden");
+        $(".submit-loading").hide();
+        $("#interestInput").hide();
+      },
+    });
+    return false;
+  });
+});
